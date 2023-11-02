204288a19ada6d125c642cde938b539521484d59
sia2230 <143744775+sia2230@users.noreply.github.com>
2023-11-02 12:34:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let displayValue = "0";
+
+function updateDisplay() {
+    document.getElementById("display").innerText = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === "0" && value !== ".") {
+        displayValue = "";
+    }
+    displayValue += value;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = "0";
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+        updateDisplay();
+    } catch (error) {
+        displayValue = "Error";
+        updateDisplay();
+    }
+}
+
+updateDisplay();
